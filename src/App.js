959925b83e3acb27018a9cb30f71e1ab3fc777fc
sohanagashimani <sohanagashimanii@gmail.com>
2023-03-26 18:39:15 +0530

    add: base url env

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import Navbar from "./modules/Navbar/Navbar";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const baseUrl = process.env.REACT_APP_BASE_URL || "/";
   return (
-    <HashRouter basename="/e-commerce">
+    <HashRouter basename={baseUrl}>
       <div>
         <Navbar />
         <ToastContainer />
